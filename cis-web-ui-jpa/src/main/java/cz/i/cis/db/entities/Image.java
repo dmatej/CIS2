089574670254282386ce8fc1ce2a6cd0cb7cb03f
svaby <svaby@svaby-PC>
2013-04-06 00:44:25 +0200

    Entita Image: úprava serializace atributu data typu Object

--- cis-web-ui-jpa/src/main/java/cz/i/cis/db/entities/Image.java
+++ cis-web-ui-jpa/src/main/java/cz/i/cis/db/entities/Image.java
@@ -2,48 +2,64 @@ package cz.i.cis.db.entities;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import java.io.*;
 
 
 /**
  * The persistent class for the image database table.
- * 
+ *
  */
 @Entity
 public class Image implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	private int id;
+    @Id
+    private int id;
 
-	private Object data;
+    @Transient private Object data;
 
-	private int idevidence;
+    @Lob private byte[] binData;
 
-	public Image() {
-	}
+    private int idevidence;
 
-	public int getId() {
-		return this.id;
-	}
+    public Image() {
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return this.id;
+    }
 
-	public Object getData() {
-		return this.data;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setData(Object data) {
-		this.data = data;
-	}
+    public Object getData() {
+        try {
+            return this.data !=null ? this.data : (this.data = new ObjectInputStream(new ByteArrayInputStream(this.binData)).readObject());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public int getIdevidence() {
-		return this.idevidence;
-	}
+    public void setData(Object data) {
+        this.data = data;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            final ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(data);
+            oos.close();
+            this.binData = baos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public void setIdevidence(int idevidence) {
-		this.idevidence = idevidence;
-	}
+    public int getIdevidence() {
+        return this.idevidence;
+    }
+
+    public void setIdevidence(int idevidence) {
+        this.idevidence = idevidence;
+    }
 
 }
