2b9cbca1395d96a30cfcfd4a2b0be3d6c55bf27b
stulc <martin.stulc@gmail.com>
2013-05-21 18:00:12 +0200

    vyhledávání identit dle parametrů

--- cis-web-ui-jpa/src/main/java/cz/i/cis/db/person/IdentityService.java
+++ cis-web-ui-jpa/src/main/java/cz/i/cis/db/person/IdentityService.java
@@ -8,13 +8,18 @@ import cz.i.cis.db.entities.Identity;
 
 @Local
 public interface IdentityService {
-    public Identity create(Identity identity);
-    public Identity update(Identity identity);
-    public Identity delete(Identity identity);
+  public Identity create(Identity identity);
 
-    public List<Identity> findIdentitiesForPerson(Integer idPerson);
+  public Identity update(Identity identity);
 
-    public List<Identity> findActualIdentitiesOfPersons();
+  public Identity delete(Identity identity);
 
-    public Identity findConcreteIdentityForPerson(Integer idIdentity);
+  public List<Identity> findIdentitiesForPerson(Integer idPerson);
+
+  public List<Identity> findIdentitiesByParams(String firstName,
+      String lastName, Boolean isMale, String birthNumber);
+
+  public List<Identity> findActualIdentitiesOfPersons();
+
+  public Identity findConcreteIdentityForPerson(Integer idIdentity);
 }
--- cis-web-ui-jpa/src/main/java/cz/i/cis/db/person/IdentityServiceBean.java
+++ cis-web-ui-jpa/src/main/java/cz/i/cis/db/person/IdentityServiceBean.java
@@ -15,61 +15,96 @@ import cz.i.cis.db.entities.Identity;
 @Named
 public class IdentityServiceBean implements Serializable, IdentityService {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @PersistenceContext(unitName = "cis")
-    private EntityManager em;
+  @PersistenceContext(unitName = "cis")
+  private EntityManager em;
 
-    public IdentityServiceBean() {
-    }
+  public IdentityServiceBean() {
+  }
 
-    @Override
-    public Identity create(Identity identity) {
-        em.persist(identity);
-        return identity;
-    }
+  @Override
+  public Identity create(Identity identity) {
+    em.persist(identity);
+    return identity;
+  }
 
-    @Override
-    public List<Identity> findIdentitiesForPerson(Integer idPerson) {
-        final String query = "SELECT i from Identity i WHERE idperson=:personID and rstatus=0";
-        TypedQuery<Identity> query1 = em.createQuery(query, Identity.class);
-        query1.setParameter("personID", idPerson);
-        final List<Identity> ident = query1.getResultList();
+  @Override
+  public List<Identity> findIdentitiesForPerson(Integer idPerson) {
+    final String query = "SELECT i from Identity i WHERE idperson=:personID and rstatus=0";
+    TypedQuery<Identity> query1 = em.createQuery(query, Identity.class);
+    query1.setParameter("personID", idPerson);
+    final List<Identity> ident = query1.getResultList();
 
-        return ident;
-    }
+    return ident;
+  }
 
-    @Override
-    public Identity findConcreteIdentityForPerson(Integer idIdentity) {
-        final String query = "SELECT i from Identity i WHERE i.id=:idIden and rstatus=0";
-        TypedQuery<Identity> query1 = em.createQuery(query, Identity.class);
-        query1.setParameter("idIden", idIdentity);
-        final List<Identity> ident = query1.getResultList();
+  @Override
+  public Identity findConcreteIdentityForPerson(Integer idIdentity) {
+    final String query = "SELECT i from Identity i WHERE i.id=:idIden and rstatus=0";
+    TypedQuery<Identity> query1 = em.createQuery(query, Identity.class);
+    query1.setParameter("idIden", idIdentity);
+    final List<Identity> ident = query1.getResultList();
 
-        if (ident.size() != 1)
-            return null;
+    if (ident.size() != 1)
+      return null;
 
-        return ident.get(0);
-    }
+    return ident.get(0);
+  }
 
-    @Override
-    public Identity update(Identity identity) {
-        return em.merge(identity);
-    }
+  @Override
+  public Identity update(Identity identity) {
+    return em.merge(identity);
+  }
 
-    @Override
-    public List<Identity> findActualIdentitiesOfPersons() {
-        final String queryStr = "SELECT i FROM Identity i, Tduperson p WHERE i.idperson = p.id and rstatus=0";
-        TypedQuery<Identity> query = em.createQuery(queryStr, Identity.class);
-        final List<Identity> idents = query.getResultList();
+  @Override
+  public List<Identity> findActualIdentitiesOfPersons() {
+    final String queryStr = "SELECT i FROM Identity i, Tduperson p WHERE i.idperson = p.id and rstatus=0";
+    TypedQuery<Identity> query = em.createQuery(queryStr, Identity.class);
+    final List<Identity> idents = query.getResultList();
 
-        return idents;
-    }
+    return idents;
+  }
 
-    @Override
-    public Identity delete(Identity identity) {
-        identity.setRstatus(-1);
-        return update(identity);
-    }
+  @Override
+  public Identity delete(Identity identity) {
+    identity.setRstatus(-1);
+    return update(identity);
+  }
+
+  @Override
+  public List<Identity> findIdentitiesByParams(String firstName,
+      String lastName, Boolean isMale, String birthNumber) {
+    String queryStr = "SELECT i FROM identity i WHERE rstatus=0";
+    if (firstName != null)
+      queryStr += " and i.firstname LIKE :firstname";
+
+    if (lastName != null)
+      queryStr += " and i.lastname LIKE :lastname";
+
+    if (lastName != null)
+      queryStr += " and i.lastname LIKE :lastname";
+
+    if (isMale != null)
+      queryStr += " and i.sex = :sex";
+
+    if (birthNumber != null)
+      queryStr += " and i.birthnumber = :birthnumber";
+
+    TypedQuery<Identity> query = em.createQuery(queryStr, Identity.class);
+
+    if (firstName != null)
+      query.setParameter("firstname", '%' + firstName + '%');
+    if (lastName != null)
+      query.setParameter("lastname", '%' + lastName + '%');
+    if (isMale != null)
+      query.setParameter("sex", isMale ? "M" : "Z");
+    if (birthNumber != null)
+      query.setParameter("birthnumber", birthNumber);
+
+    final List<Identity> idents = query.getResultList();
+
+    return idents;
+  }
 
 }
