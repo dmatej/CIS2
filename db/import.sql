12548a21e61ff6438d4576239e48e98ff8ccad17
David Matejcek <david.matejcek@i.cz>
2013-05-27 14:14:35 +0200

    Duvody pobytu

--- db/import.sql
+++ db/import.sql
@@ -9,3 +9,7 @@ load from code_documenttype.unl insert into code_documenttype;
 
 truncate table code_permissiontype;
 load from code_permissiontype.unl insert into code_permissiontype;
+
+truncate table code_purposeofstay;
+load from code_purposeofstay.unl insert into code_purposeofstay;
+
